71228686dc1fa85fd38f7d524d372f1eb491f248
Abhas <abhasforyou@gmail.com>
2017-07-03 00:50:41 -0700

    Add error messaging to user creation page

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -15,7 +15,18 @@ defmodule Offerdate.UserController do
   def new(conn, _params) do
   	changeset = User.changeset(%User{})
   	render conn, "new.html", changeset: changeset
+  end
 
+  def create(conn, %{"user" => user_params}) do
+  	changeset = User.changeset(%User{}, user_params)
+  	case Repo.insert(changeset) do
+  		{:ok, user} -> 
+  			conn
+  			|> put_flash(:info, "#{user.first_name <> " " <> user.last_name} created!")
+  			|> redirect(to: user_path(conn, :index))
+  		{:error, changeset} -> 
+  			render conn, "new.html", changeset: changeset
+  	end
   end
 
 end
