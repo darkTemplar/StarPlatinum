a2964e56eb814a3baf9fd1d17195455e3283714c
Abhas <abhasforyou@gmail.com>
2017-07-03 23:01:48 -0700

    Add authenticate plug to show and index actions

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,19 @@
 defmodule Offerdate.UserController do
   use Offerdate.Web, :controller
+  plug :authenticate when action in [:index, :show]
   alias Offerdate.User
+
+
+  defp authenticate(conn, _opts) do
+  	if conn.assigns.current_user do
+  		conn
+  	else
+  		conn
+  		|> put_flash(:error, "You must be logged in to access that page.")
+  		|> redirect(to: page_path(conn, :index))
+  		|> halt() 
+  	end
+  end
 
   def index(conn, _params) do
   	users = Repo.all(User)
