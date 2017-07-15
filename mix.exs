64e4edf3c3da9d51ca444528c84e4e8823014181
Abhas <abhasforyou@gmail.com>
2017-07-15 16:30:29 -0700

    Add httpoison and poison as dependencies to project

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Offerdate.Mixfile do
   def application do
     [mod: {Offerdate, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex, :comeonin]]
+                    :phoenix_ecto, :mariaex, :comeonin, :httpoison, :poison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,6 +38,8 @@ defmodule Offerdate.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:comeonin, "~> 2.0"},
+     {:poison, " ~> 2.2"},
+     {:httpoison, "~> 0.9"},
      {:cowboy, "~> 1.0"}]
   end
 
