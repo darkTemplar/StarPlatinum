6617593567a6619bbe7556a44a69dabe08ae35d8
Abhas Sinha <abhasforyou@gmail.com>
2018-02-05 08:59:54 -0800

    make changes to data for all cash offers + add offer expiry var

--- data_sci/generate_offers.py
+++ data_sci/generate_offers.py
@@ -22,16 +22,19 @@ def read_listings_from_file(filename):
 def generate_random_offer(list_price):
     offer = {'list_price': list_price}
     price_fluctuation = random.uniform(-0.3, 0.3)
-    escrow_fluctuation = random.uniform(-0.3, 0.3)
+    escrow_fluctuation = random.uniform(-0.8, 2)
+    offer_expiry_fluctuation = random.uniform(-0.8, 0.8)
     # financing (vars)
     offer['offer_price'] = ceil((1 + price_fluctuation) * list_price) # round off to nearest int
     offer['all_cash'] = random.choice(['Y','N']) # boolean
-    offer['proportion_down_payment'] = random.random() # percentage between 0 and 1
-    offer['financing_contingency'] = random.choice(['Y','N']) # boolean
-    offer['appraisal_contingency'] = random.choice(['Y','N']) # boolean
+    proportion_down_payment = random.random() if offer['all_cash'] == 'N' else 0.0 # percentage
+    offer['proportion_down_payment'] = round(proportion_down_payment, 2)
+    
 
     # financing terms (do we need more variables)
-    offer['preapproved_loan'] = random.choice(['Y','N']) # boolean
+    offer['financing_contingency'] = random.choice(['Y','N']) # boolean
+    offer['appraisal_contingency'] = random.choice(['Y','N'])
+    offer['preapproved_loan'] = random.choice(['Y','N']) if offer['all_cash'] == 'N' else 'N' # boolean
 
     # escrow terms 
     offer['escrow_period'] = ceil(30 * (1 + escrow_fluctuation))  # days
@@ -49,7 +52,7 @@ def generate_random_offer(list_price):
     offer['num_disclosures_signed'] = random.randint(0, 5) # int
     
     # other terms
-    offer['accept_liquid_damages'] = random.choice(['Y','N']) # boolean
+    offer['offer_expiry'] = ceil(3 * (1 + offer_expiry_fluctuation)) # days
     offer['pays_for_home_warranty'] = random.choice(['Y','N']) # boolean
     return offer
 
@@ -57,7 +60,7 @@ def write_offers(offers):
     with open('offers.csv', 'w') as csvfile:
         field_names = ['list_price', 'offer_price', 'all_cash', 'proportion_down_payment', 'financing_contingency', 'appraisal_contingency', 
                 'preapproved_loan', 'escrow_period', 'pays_title_insurance', 'chooses_title_insurance', 'pays_escrow_fees', 'chooses_escrow_company',
-                'property_as_is', 'property_inspection_contingency', 'pest_control', 'repairs_payment', 'num_disclosures_signed', 'accept_liquid_damages',
+                'property_as_is', 'property_inspection_contingency', 'pest_control', 'repairs_payment', 'num_disclosures_signed', 'offer_expiry',
                 'pays_for_home_warranty']
         
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
