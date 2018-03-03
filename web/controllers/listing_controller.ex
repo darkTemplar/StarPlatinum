2aaaafe0d88b5dfbbad531c1676b57add9d9ac45
Abhas Sinha <abhasforyou@gmail.com>
2018-03-03 13:19:23 -0800

    First draft of api for creating listings

---
+++ web/controllers/listing_controller.ex
@@ -0,0 +1,54 @@
+defmodule Offerdate.ListingController do
+  use Offerdate.Web, :controller
+  # plug(:authenticate_user when action in [:index, :show])
+  alias Offerdate.Auth
+  alias Offerdate.Listing
+
+  def show(conn, %{"id" => listing_id}) do
+    listing = Repo.get(Listing, String.to_integer(listing_id))
+    render(conn, "show.json", user: listing)
+  end
+
+  def new(conn, _params) do
+    changeset = Listing.changeset(%Listing{})
+    render(conn, "new.json", changeset: changeset)
+  end
+
+  def create(conn, %{"listing" => listing_params}) do
+    changeset = Listing.changeset(%Listing{}, listing_params)
+
+    case Repo.insert(changeset) do
+      {:ok, listing} ->
+        conn
+        |> redirect(to: listing_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.json", changeset: changeset)
+    end
+  end
+
+  def update(conn, %{"id" => listing_id, "listing" => listing_params}) do
+    listing = Repo.get(Listing, String.to_integer(listing_id))
+    changeset = Listing.changeset(listing, listing_params)
+
+    case Repo.update(changeset) do
+      {:ok, listing} ->
+        render(conn, "show.json", listing: listing)
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(conn, "error.json", changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => listing_id}) do
+    listing = Repo.get!(Listing, String.to_integer(listing_id))
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(listing)
+
+    send_resp(conn, :no_content, "")
+  end
+end
--- web/router.ex
+++ web/router.ex
@@ -2,38 +2,36 @@ defmodule Offerdate.Router do
   use Offerdate.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-    plug Offerdate.Auth, repo: Offerdate.Repo
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(Offerdate.Auth, repo: Offerdate.Repo)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
-    plug :fetch_session
-    plug :fetch_flash
+    plug(:accepts, ["json"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
   end
 
   pipeline :api_auth do
-    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-    plug Guardian.Plug.LoadResource
+    plug(Guardian.Plug.VerifyHeader, realm: "Bearer")
+    plug(Guardian.Plug.LoadResource)
   end
 
-
-  scope "/api", JwtExample do
-    pipe_through [ :api, :api_auth ]
-    resources "/users", UserApiController, only: [:index, :show, :new, :create]
-    resources "/listings", ListingController, only: [:new, :show, :edit, :delete]
+  scope "/api", Offerdate do
+    pipe_through([:api, :api_auth])
+    resources("/users", UserApiController, only: [:index, :show, :new, :create])
+    resources("/listings", ListingController, only: [:new, :show, :edit, :delete])
   end
 
   scope "/", Offerdate do
-    pipe_through :browser
-    get "/", PageController, :index
-    resources "/users", UserController, only: [:index, :show, :new, :create]
-    resources "/sessions", SessionController, only: [:new, :create, :delete]
-
+    pipe_through(:browser)
+    get("/", PageController, :index)
+    resources("/users", UserController, only: [:index, :show, :new, :create])
+    resources("/sessions", SessionController, only: [:new, :create, :delete])
   end
 
   # Other scopes may use custom stacks.
