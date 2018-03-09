53e1ab4bf6e32c8969957178bafe9c442863a000
Abhas Sinha <abhasforyou@gmail.com>
2018-03-09 00:07:54 -0800

    Add token serializer to support guardian module

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :offerdate,
-  ecto_repos: [Offerdate.Repo]
+config :offerdate, ecto_repos: [Offerdate.Repo]
 
 # Configures the endpoint
 config :offerdate, Offerdate.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "o0mxuUyfsnhKZDxgr/vN17X2yR2rQevQpqxbR9ixmePKSSiYE7aX/yL/RyfQoUxi",
   render_errors: [view: Offerdate.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Offerdate.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Offerdate.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,12 +22,14 @@ config :logger, :console,
 
 config :guardian, Guardian,
   allowed_algos: ["ES512"],
+  verify_module: Guardian.JWT,
   secret_key: "2+0TlcEP3286M/svrILhZNGer7+URHjnm7awCMMbhV7DRSYCC+VccOk2FFX5NO8s",
   issuer: "Offerdate",
   verify_issuer: true,
-  ttl: { 30, :days },
+  ttl: {30, :days},
+  allowed_drift: 2000,
   serializer: Offerdate.GuardianSerializer
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
--- mix.exs
+++ mix.exs
@@ -2,47 +2,65 @@ defmodule Offerdate.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :offerdate,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :offerdate,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {Offerdate, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex, :comeonin, :httpoison, :poison, :exredis]]
+    [
+      mod: {Offerdate, []},
+      applications: [
+        :phoenix,
+        :phoenix_pubsub,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :phoenix_ecto,
+        :mariaex,
+        :comeonin,
+        :httpoison,
+        :poison,
+        :exredis
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_), do: ["lib", "web"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.4"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:mariaex, ">= 0.0.0"},
-     {:exredis, ">= 0.2.4"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:comeonin, "~> 2.0"},
-     {:guardian, "~> 0.14"},
-     {:poison, " ~> 2.2"},
-     {:httpoison, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.2.4"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:mariaex, ">= 0.0.0"},
+      {:exredis, ">= 0.2.4"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:comeonin, "~> 2.0"},
+      {:guardian, "~> 0.14"},
+      {:poison, " ~> 2.2"},
+      {:httpoison, "~> 0.9"},
+      {:cowboy, "~> 1.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
@@ -52,8 +70,10 @@ defmodule Offerdate.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
   end
 end
