fbbdc8af4d2e89146ced11f0d230dfe2e011d7a1
Auster Chen <auster.s.chen@gmail.com>
2018-03-19 00:02:13 -0700

    don't always show suggestion

--- app/shared/components/Geocomplete/index.js
+++ app/shared/components/Geocomplete/index.js
@@ -25,7 +25,6 @@ export default class Geocomplete extends React.PureComponent {
         getSuggestionValue={suggestion => suggestion.description}
         fetchSuggestions={fetchSuggestions}
         inputProps={inputProps}
-        alwaysRenderSuggestions
       />
     );
   }
