208b34cff808310dd1a6a99d9db029352be0a581
Auster Chen <auster.s.chen@gmail.com>
2018-04-01 22:12:43 -0700

    add custom build folder

--- app/next.config.js
+++ app/next.config.js
@@ -1,2 +1,5 @@
+const _assignIn = require('lodash/assignIn');
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS();
+module.exports = _assignIn({}, withCSS(), {
+  distDir: 'build',
+});
