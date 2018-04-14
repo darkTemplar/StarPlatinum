fbea9d5d008e177c6aa01422c67edd061eb1bd72
Abhas Sinha <abhasforyou@gmail.com>
2018-04-14 07:54:39 -0700

    Add kubernetes configs

--- rel/config.exs
+++ rel/config.exs
@@ -7,14 +7,13 @@ Path.join(["rel", "plugins", "*.exs"])
 |> Enum.map(&Code.eval_file(&1))
 
 use Mix.Releases.Config,
-    # This sets the default release built by `mix release`
-    default_release: :default,
-    # This sets the default environment used by `mix release`
-    default_environment: Mix.env()
+  # This sets the default release built by `mix release`
+  default_release: :default,
+  # This sets the default environment used by `mix release`
+  default_environment: Mix.env()
 
 # For a full list of config options for both releases
 # and environments, visit https://hexdocs.pm/distillery/configuration.html
-
 
 # You may define one or more environments in this file,
 # an environment's settings will override those of a release
@@ -28,15 +27,15 @@ environment :dev do
   # It is recommended that you build with MIX_ENV=prod and pass
   # the --env flag to Distillery explicitly if you want to use
   # dev mode.
-  set dev_mode: true
-  set include_erts: false
-  set cookie: :"9xrXsj2Nkc$s6[r^C}.*2tHJQp.LRDnj<!/?x}@~RW[4yOEq0&3P3YJ@.A])H~W6"
+  set(dev_mode: true)
+  set(include_erts: false)
+  set(cookie: :"9xrXsj2Nkc$s6[r^C}.*2tHJQp.LRDnj<!/?x}@~RW[4yOEq0&3P3YJ@.A])H~W6")
 end
 
 environment :prod do
-  set include_erts: true
-  set include_src: false
-  set cookie: :"uhqE`?C;*TB,P*MiqHUlo%5S2bj=C4e&Dy,8q_&hl~Xyn/gszvlJrY?r2.h=Ega1"
+  set(include_erts: true)
+  set(include_src: false)
+  set(cookie: :"uhqE`?C;*TB,P*MiqHUlo%5S2bj=C4e&Dy,8q_&hl~Xyn/gszvlJrY?r2.h=Ega1")
 end
 
 # You may define one or more releases in this file.
@@ -46,8 +45,11 @@ end
 
 release :offerdate do
   set version: current_version(:offerdate)
-  set applications: [
-    :runtime_tools
-  ]
-end
+  set vm_args: "./vm.args"
 
+  set(
+    applications: [
+      :runtime_tools
+    ]
+  )
+end
