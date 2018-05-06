87aa6c919b2b84113e03b81d6c932ff2343d0a8e
Abhas Sinha <abhasforyou@gmail.com>
2018-05-06 16:16:02 -0700

    script to build images, tag them and push to amazon container registry

---
+++ aws_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "********************************************************"
+echo "Build and push docker images to Amazon Container Registry"
+echo "********************************************************"
+echo "Build images"
+docker-compose build
+echo "********************************************************"
+echo "login to registry"
+`aws ecr get-login --no-include-email`
+echo "********************************************************"
+echo "tag images"
+docker tag phoenix:latest 103126352378.dkr.ecr.us-west-1.amazonaws.com/phoenix:latest
+docker tag web-client:latest 103126352378.dkr.ecr.us-west-1.amazonaws.com/web-client:latest
+echo "********************************************************"
+echo "push images"
+docker push 103126352378.dkr.ecr.us-west-1.amazonaws.com/offerdate:phoenix
+docker push 103126352378.dkr.ecr.us-west-1.amazonaws.com/web-client:web-client
+echo "********************************************************"
+echo "Done"
+echo "********************************************************"
