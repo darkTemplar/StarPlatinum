953628d672c4668a2e1de7f4e06b1bf57de72074
Abhas Sinha <abhasforyou@gmail.com>
2018-05-22 09:32:35 -0700

    Change db host name

--- entry.sh
+++ entry.sh
@@ -4,7 +4,7 @@
 # Docker entry script.
 
 # Wait until Postgres is ready
-while ! pg_isready -h $DB_HOST -U $DB_USERNAME
+while ! pg_isready -h $POSTGRES_HOST -U $POSTGRES_USER
 do
   echo "$(date) - waiting for database to start"
   sleep 2
@@ -13,7 +13,7 @@ done
 # Create, migrate, and seed database if it doesn't exist.
 if [[ -z `psql -Atqc "\\list $DB_NAME"` ]]; then
   echo "Database $DB_NAME does not exist. Creating..."
-  createdb -E UTF8 $DB_NAME -l en_US.UTF-8 -T template0
+  createdb -E UTF8 $POSTGRES_DATABASE -l en_US.UTF-8 -T template0
   mix ecto.migrate
   mix run priv/repo/seeds.exs
   echo "Database $DB_NAME created."
